f4017d18c3bd19d9466af3ef7995e8bea103ba5a
yonghong.liu <dou.liu@nf-3.com>
2018-10-09 20:20:28 +0800

    1、添加了有关的定位和WiFi的扫描功能；

---
+++ DouStudy/app/src/main/java/com/ws/yonghong/doustudy/activity/LocationActivity.java
@@ -0,0 +1,112 @@
+package com.ws.yonghong.doustudy.activity;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ws.yonghong.doustudy.R;
+import com.ws.yonghong.doustudy.View.RipplesView;
+import com.ws.yonghong.doustudy.task.AbsTask;
+import com.ws.yonghong.doustudy.task.TaskManager;
+import com.ws.yonghong.doustudy.utilcode.util.LogUtils;
+import com.ws.yonghong.doustudy.utilcode.util.TimeUtils;
+import com.ws.yonghong.doustudy.utilcode.util.ToastUtils;
+
+import java.util.List;
+
+public class LocationActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView tv_show;
+    private Button btn_get_provider;
+    private Button btn_get;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        btn_get_provider = (Button) findViewById(R.id.btn_get_provider);
+        btn_get = (Button) findViewById(R.id.btn_get);
+        btn_get_provider.setOnClickListener(this);
+        btn_get.setOnClickListener(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            initData();
+        }
+    }
+
+
+    public void start(View v) {
+
+    }
+
+    public void stop(View v) {
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_get:
+                ToastUtils.showShort("btn_get");
+                break;
+            case R.id.btn_get_provider:
+                ToastUtils.showShort("btn_get_provider");
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+    private void initData() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> list = locationManager.getProviders(true);
+        if (list != null) {
+            for (String x : list) {
+                LogUtils.e("gzq", "name:" + x);
+            }
+        }
+
+        LocationProvider lpGps = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        LocationProvider lpNet = locationManager.getProvider(LocationManager.NETWORK_PROVIDER);
+        LocationProvider lpPsv = locationManager.getProvider(LocationManager.PASSIVE_PROVIDER);
+
+
+        Criteria criteria = new Criteria();
+        // Criteria是一组筛选条件
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        //设置定位精准度
+        criteria.setAltitudeRequired(false);
+        //是否要求海拔
+        criteria.setBearingRequired(true);
+        //是否要求方向
+        criteria.setCostAllowed(true);
+        //是否要求收费
+        criteria.setSpeedRequired(true);
+        //是否要求速度
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        //设置电池耗电要求
+        criteria.setBearingAccuracy(Criteria.ACCURACY_HIGH);
+        //设置方向精确度
+        criteria.setSpeedAccuracy(Criteria.ACCURACY_HIGH);
+        //设置速度精确度
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        //设置水平方向精确度
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+        //设置垂直方向精确度
+
+        //返回满足条件的当前设备可用的provider，第二个参数为false时返回当前设备所有provider中最符合条件的那个provider，但是不一定可用
+        String mProvider = locationManager.getBestProvider(criteria, true);
+        if (mProvider != null) {
+            Log.e("gzq", "mProvider:" + mProvider);
+        }
+    }
+}
